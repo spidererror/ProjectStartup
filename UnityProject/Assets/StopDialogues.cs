896a07955dc6a629019a1223e3aa2b75c58b9233
Kristýna Pavlátová <KristynaPavlatova@users.noreply.github.com>
2021-02-12 17:27:25 +0100

    Menu + buttons, Settings + buttons

---
+++ UnityProject/Assets/StopDialogues.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class StopDialogues : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// Stops a whole flowchart
+    /// </summary>
+    public void StopFlowchart()
+    {
+        StopFlowchart();
+    }
+}
