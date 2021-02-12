3c80017df4bd3f2ad6b9ec6260580209ab355155
Niels Prins <447730@student.saxion.nl>
2021-02-12 02:05:24 +0100

    Phone dialogue is working just some tweaking and asset replacement.

--- UnityProject/Assets/scripts/Niels/PhoneDialogueScripts/DuplicatePanel.cs
+++ UnityProject/Assets/scripts/Niels/PhoneDialogueScripts/DuplicatePanel.cs
@@ -1,47 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.UI;
 public class DuplicatePanel : MonoBehaviour
 {
     public Text textChange;
+    public GameObject gameObjectInactive;
     public RectTransform objectToDuplicate;
     public Transform parent;
-    public float offset = 20;
-    private int textLength;
+    public float offsetY = 20;
+    public float offsetX = 50;
+    private string _currentText;
+    private bool _pressedButton;
+    private List<RectTransform> _allDuplicates = new List<RectTransform>();
 
-    private List<RectTransform> allDuplicates = new List<RectTransform>();
-
-    private void Start()
-    {
-        textLength = textChange.text.Length;
-    }
 
     private void Update()
     {
         duplicate();
     }
 
+    public void DuplicateOnClick()
+    {
+
+        _allDuplicates.Add(Instantiate(objectToDuplicate, parent));
+        positoinPreviousDuplicates();
+
+        _currentText = textChange.text;
+        _pressedButton = true;
+    }
+
     private void duplicate()
     {
-        string currentText = textChange.text;
-        if (currentText.Length != textLength)
+        if (_currentText != textChange.text && Input.GetButtonDown("Fire1") && !gameObjectInactive.activeSelf)
         {
-            Instantiate(objectToDuplicate, parent);
-            //positoinPreviousDuplicates();
-            allDuplicates.Add(objectToDuplicate);
-            textLength = currentText.Length;
+            _allDuplicates.Add(Instantiate(objectToDuplicate, parent));
+            positoinPreviousDuplicates();
+
+            _currentText = textChange.text;
         }
     }
 
     private void positoinPreviousDuplicates()
     {
-        for (int i = 0; i < allDuplicates.Count; i++)
+        
+        for (int i = 0; i < _allDuplicates.Count; i++)
         {
-            if (i > 0)
+            if (i<_allDuplicates.Count-1)
             {
-                allDuplicates[i].transform.position = new Vector3(allDuplicates[i].transform.position.x, allDuplicates[i].transform.position.y + offset, allDuplicates[i].transform.position.z);
+                _allDuplicates[i].transform.position = new Vector3(_allDuplicates[i].transform.position.x, _allDuplicates[i].transform.position.y + offsetY+(_allDuplicates[i+1].rect.height/5), _allDuplicates[i].transform.position.z);
+
             }
+            else
+            {
+                float newX = 0;
+                if (_pressedButton)
+                {
+                    newX = offsetX;
+                    _pressedButton = false;
+                }
+                _allDuplicates[i].transform.position = new Vector3(_allDuplicates[i].transform.position.x+newX, _allDuplicates[i].transform.position.y + offsetY, _allDuplicates[i].transform.position.z);
+            }
+
 
         }
     }
